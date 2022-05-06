192de7cfc3d05cbf506e0a95239fa12fd4f691d3
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-05-06 01:03:53 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,22 @@
-import { Avatar, Box, Flex,Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align='center' >
-       
-            <Box mr='4' textAlign='right' >
+            {showProfileData && (
+                <Box mr='4' textAlign='right' >
                 <Text>Israel Tavares</Text>
                 <Text
                     color='gray.300'
                     fontSize='small'>
                     israelbulcaotavares@gmail.com</Text>
             </Box>
-
+            )}
+             
             <Avatar size='md' name='Israel Tavares' src='https://github.com/israelbulcaotavares.png' />
 
         </Flex>
