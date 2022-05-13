6a3cc4eb2747b414f9935b77f6bc934c19fbba72
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-05-13 00:30:41 -0300

    Validação dos dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Input } from '../components/Form/Input'
+import { Input } from '../components/Form/Input';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+
 
 type SignInFormData = {
   email: string;
   password: string;
 };
 
+const signInFormSchema = yup.object({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+}).required();
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState: { errors }, formState } = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  
+  
+
+
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000));
@@ -26,6 +42,7 @@ export default function SignIn() {
       h='100vh'
       align='center'
       justify='center'>
+
       <Flex
         as="form"
         width="100%"
@@ -38,8 +55,19 @@ export default function SignIn() {
 
       >
         <Stack spacing='4' >
-          <Input name='email' type='email' label='E-mail' {...register('email')} />
-          <Input name='password' type='password' label='Senha' {...register('password')} />
+          <Input
+            name='email'
+            type='email'
+            label='E-mail' 
+            error={errors.email}
+            {...register(('email'), {required: 'E-mail obrigatório'})} />
+            
+          <Input
+            name='password'
+            type='password'
+            label='Senha' 
+            error={errors.password}
+            {...register(('password'),{required: 'Senha obrigatória'})} />
 
 
         </Stack>
